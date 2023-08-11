0a4b3ae74f948a32895bfff80977ebb911916f38
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-10 17:57:08 -0700

    crazy betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
