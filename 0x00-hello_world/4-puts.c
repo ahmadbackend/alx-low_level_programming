984af04ae1c34b47f1f678db4c73b12fd59a1600
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-10 17:29:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include<stdio.h>
+#include <string.h>
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return 0;
+}
