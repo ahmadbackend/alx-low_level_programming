0a57d12522a3fc5082d9836516fa680e731859b6
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-10 19:44:00 -0700

    fr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+        {
+		char twist[83] = "and that piece of art is useful\" ";
+		
+		char twist2[] = "- Dora Korpar, 2015-10-19";
+		
+		strcat(twist, twist2);
+		fprintf(stderr, "%s", twist);
+		return (1);
+	}
