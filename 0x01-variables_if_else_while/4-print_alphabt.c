73e5c2045bc3c5c6de878fca432d74f0c6faef60
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-11 14:16:31 -0700

    trt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+	{
+		char letter = 'a';
+
+		for (letter = 'a'; letter <= 'z' ; letter++)
+		{
+			if (letter != 'q' && letter != 'e')
+			{
+				putchar(letter);
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
