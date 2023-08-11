e69e30452becd8f47e1b1368fb7914810186ce5b
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-11 14:30:59 -0700

    trs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+	{
+		char i = '0';
+
+		for (i = '0'; i < '10' ; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+		return (0);
+	}
+
