b9a5f5df0ab76c57ecb52cfecb85d36f3d0edbb8
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-11 11:03:21 -0700

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,7 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-        {
-		char twist[83] = "and that piece of art is useful\" ";
-		
-		char twist2[] = "- Dora Korpar, 2015-10-19";
-		
-		strcat(twist, twist2);
-		fprintf(stderr, "%s", twist);
-		return (1);
+	{
+		fprintf(stderr, "%s%s", "and that piece of art is useful\"",
+				" - Dora Korpar, 2015-10-19");
 	}
