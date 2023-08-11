59fc45d7184595901bede0defcfb605d9bd135a7
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-11 14:20:01 -0700

    rty

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			printf(i);
+		}
+		printf("\n");
+		return (0);
+	}
