c71c51c502d95d53c0577c1223bfdbb5aa45d818
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-10 17:48:27 -0700

    psycho

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle \n");
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
