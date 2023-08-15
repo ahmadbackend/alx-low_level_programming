add19336b8f6d4c06d6dfe83996ae49c297a360e
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-15 12:02:34 -0700

    hytr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -9,7 +10,7 @@ int main(void)
 		char str[] = "_putchar";
 		int ch;
 
-		for (ch = 0; ch < strlen(str); ch++)
+		for (ch = 0; ch < 8; ch++)
 			_putchar(str[ch]);
 		_putchar('\n');
 		return (0);
