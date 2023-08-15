84e24aafff93eee2dcd2f68265128c6b4153e673
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-15 12:21:29 -0700

    Grt

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print ten times alphapets
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+	{
+		int ch;
+		int ten;
+
+		for (ten = 0 ; ten < 10 ; ten++)
+		{
+			for (ch = 'a' ; ch <= 'z' ; ch++)
+			{
+				_putchar(ch);
+			}
+		}
+		_putchar('\n');
+	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _putchar(char c);
 #endif
