75edbd8339d34185b68c6f36a8e98e9dec1eba35
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-17 11:52:04 -0700

    fds

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,10 +7,14 @@
 void print_diagonal(int n)
 	{
 		int cha;
+		int spac;
 
-		for (cha = 0 ; cha < n ; cha++)
-			_putchar(' ');
-		if (n > 0)
-			_putchar(92);
+		for (cha = 1 ; cha <= n ; cha++)
+		{
+			for (spac = 0 ; spac < cha ; spac++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 		_putchar('\n');
 	}
