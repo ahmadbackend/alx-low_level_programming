3d77e33993aac24a826283f3e4330371995297e2
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-17 08:25:26 -0700

    Trew

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers- Entry point
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+	{
+		int i;
+
+		for (i = '0' ; i <= '9' ; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
