cddaae2ab04f263453e1696489a008cc60541641
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-17 09:32:51 -0700

    gtre

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * more_numbers- Entry point
+ * Return: Always 0 (Success)
+*/
+void more_numbers(void)
+	{
+		int lines;
+		int dig;
+		int firstP;
+		int remin;
+
+		remin = -1;
+		for (lines = 0 ; lines < 10; lines++)
+		{
+			for (dig = 0 ; dig <= 14 ; dig++)
+			{
+				if (dig > 10)
+				{
+					firstP = dig / 10;
+					remin = dig % 10;
+				}
+				else
+				{
+					firstP = dig;
+				}
+				_putchar('0' + firstP);
+				if (remin != -1)
+					_putchar('0' + remin);
+			}
+			_putchar('\n');
+		}
+	}
