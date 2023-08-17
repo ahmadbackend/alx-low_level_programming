3b82e6329a53cbce210ebe67a8bc6677e516b424
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-17 12:59:06 -0700

    gd

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * FizzBuzz- Entry point
+ * Return: Always 0 (Success)
+*/
+void FizzBuzz(void)
+	{
+		int start;
+
+		for (start = 1 ; start <= 100; start++)
+		{
+			if (start % 3 == 0)
+				printf("%s", "Fizz");
+			if (start % 5 == 0)
+				printf("%s", "Buzz");
+			else
+				printf("%d", start);
+			_putchar(' ');
+		}
+		printf("\n");
+	}
