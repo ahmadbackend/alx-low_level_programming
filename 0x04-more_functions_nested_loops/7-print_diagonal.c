c54f577bb1383b2f1a6edf449186555acfd4589f
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-17 11:24:52 -0700

    Gfds

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal- Entry point
+ *@n: the diagonal length
+ * Return: Always 0 (Success)
+*/
+void print_diagonal(int n)
+	{
+		int cha;
+		int spac;
+
+		for (cha = 0 ; cha < n ; cha++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
