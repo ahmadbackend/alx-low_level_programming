2075cdb22f145f8184d036f49e86bd732a47bdb7
ahmadbackend <107940707+ahmadbackend@users.noreply.github.com>
2023-08-22 00:44:07 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code 
- *@*n: to be inverted to 98
+ * reset_to_98 - check the code
+ *@n: to be inverted to 98
  * Return: Always 0.
  */
 void reset_to_98(int *n)
-    {
-        *n = 98;
-    }
+{
+    *n = 98;
+}
