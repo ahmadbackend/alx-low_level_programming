3a6d4a377dd5656fe54970c48d91aa857661f929
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-22 02:48:39 +0300

    gytr

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,10 @@ void rev_string(char *s)
 		for (i = 0; *s != '\0'; s++)
 			i++;
 
-		for (l = 0; l < i / 2; i++)
+		for (l = 0; l < i / 2; l++)
 		{
 			temp = s[l];
-			s[l] = s[i - 1 -l];
+			s[l] = s[i - 1 - l];
 			s[i - 1 - l] = temp;
 		}
 	}
