9ba38e3d25cbc754fca2b2caf4de4384d591bee9
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-22 02:22:41 +0300

    Gtr

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *@s: string to be printed reversely
+ * Return: Always 0.
+*/
+void print_rev(char *s)
+	{
+		int countEnd;
+
+		for (countEnd = 0 ; *s != '\0' ; *s++)
+			countEnd++;
+		while (countEnd >= 0)
+		{
+			_putchar(s[countEnd]);
+			countEnd--;
+		}
+	}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
 #endif
