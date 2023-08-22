6760312edf0fd7b33438ad55863c9b906ff5bd84
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-22 22:37:11 +0300

    Gfd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 		while (str[length] != '\0')
 			length++;
 		if (length % 2 != 0)
-			counter = (length - 1) / 2;
+			counter = (length + 1) / 2;
 		else
 			counter = length / 2;
 		while (str[counter] != '\0')
