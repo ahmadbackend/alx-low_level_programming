245902a5bb479d29421f53d94cae747c8c5d0223
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-22 22:19:02 +0300

    Gd

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - check the code
+ *@str: the character to be printed
+ * Return: Always 0.
+*/
+void puts2(char *str)
+	{
+		int cha;
+
+		cha = 0;
+		while (str[cha] != '\0')
+		{
+			if (cha % 2 == 0)
+				_putchar(str[cha]);
+			cha++;
+		}
+	}		
