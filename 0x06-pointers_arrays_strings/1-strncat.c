422bf999af5432e004748e56229216d99584eac5
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-24 22:56:44 +0300

    Hdre

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 /**
  * _strncat - check the code
  *@n: size t obe copied
@@ -9,15 +10,20 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		int disSize;
 		int copy;
+		bool isEnded;
 		
 		disSize = 0;
 		copy = 0;
+		isEnded = false;
 		while (dest[disSize] != '\0')
 			disSize++;
-		while (disSize < n && src[copy] != '\0')
+		while (disSize < n && isEnded != true)
 		{
 			dest[disSize + 1] = src[copy];
+			if (src[copy] == '\0')
+				isEnded == true;
 			copy++;
+			
 		}
 		return (dest);
 	}
