1390720d871f49d1d32112466bd360a3e6b3970d
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-25 16:46:47 +0300

    Hyt

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,17 @@
 char *_strncpy(char *dest, char *src, int n)
 	{
 		int c;
+		int lenSrc;
 
+		lenSrc = 0;
+		while (src[lenSrc])
+			lenSrc++;
 		for (c = 0 ; c < n; c++)
 		{
-			dest[c] = src[c];
-			if (src[c] == '\0')
-				break;
+			if (c >= lenSrc)
+				dest[c] = '\0';
+			else
+				dest[c] = src[c];
 		}
 		return (dest);
 	}
