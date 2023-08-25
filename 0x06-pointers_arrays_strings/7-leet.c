17588a5129e8a813454457c6a47229cf2e625e1e
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-25 03:47:25 +0300

    Hg

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,21 +5,17 @@
 */
 char *leet(char *str)
 	{
-		int counter, i, j;
+		int counter, i;
 		char replacer[5][2] = {{'a', '4'}, {'e', '3'},
 			{'o', '0'}, {'t', '7'}, {'l', '1'}};
 
-		counter = 0;
-		while (str[counter] != '\0')
+		for (counter = 0; str[counter] != '\0'; counter++)
 		{
 			for (i = 0 ; i < 5 ; i++)
 			{
 				if (str[counter] == replacer[i][0] || str[counter] + 32 == replacer[i][0])
-				{
 					str[counter] = replacer[i][1];
-				}
 			}
-			counter++;
 		}
 		return (str);
 	}
