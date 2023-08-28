c3c580e350794be3cfa89bdb74c02a45da90aa0e
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-28 22:48:40 +0300

    Gtd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,27 @@
 */
 char *cap_string(char *str)
 	{
-		int i;
+		int i, j;
 		bool isFound;
+		char spaces[] = " ;.!?\"(){}\n\t,";
 
 		isFound = false;
 		for ( i = 0 ; str[i] != '\0'; i++)
 		{
-			if (str[i] >= 97 && str[i] <= 122 &&
-					(isFound || i ==0))
+			for (j = 0 ; j < 13 ; j++)
+			{
+				if (str[i] == spaces[j])
+				{
+					isFound = true;
+					break;
+				}
+			}
+			if ((i == 0 || isFound == true) && (str[i] >= 97 && str[i] <= 122))
 			{
 				str[i] = str[i] - 32;
 				isFound = false;
 			}
-			if (!isFound && (str[i] == ',' || str[i] ==':'
-						|| str[i] == '.'
-						|| str[i] == ' '
-						|| str[i] == '!'
-						|| str[i] == '?'
-						|| str[i] == '"'
-						|| str[i] == '('
-						|| str[i] == ')'
-						|| str[i] =='{'
-						|| str[i] =='}'))
-				isFound = true;
+			isFound = false;
 		}
 		return (str);
 	}
---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - prints buffer in hexa
+ *@n: the amount to be filled
+ *@s: the address of memory to print
+ *@b the character to be printed
+ *
+ * Return: Nothing.
+*/
+char *_memset(char *s, char b, unsigned int n)
+	{
+		unsigned int i;
+
+		i = 0;
+		while ( i < n)
+		{
+			*(s+i) = b;
+			i++;
+		}
+		return (s);
+	}
