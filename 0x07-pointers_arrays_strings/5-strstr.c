2d58e3a3d7d348c0c50e51280a4b26f47b912a6b
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-29 01:58:27 +0300

    dfg

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  *@haystack: the stack to find the string
  *@needle: the wanted string
  * Return: Always 0.
-I*/
+ */
 char *_strstr(char *haystack, char *needle)
 	{
 		int i, j, min;
@@ -12,10 +12,10 @@ char *_strstr(char *haystack, char *needle)
 
 		isMatch = false;
 		min = -1;
-		for ( i = 0 ; haystack[i] != '\0'; i++)
+		for (i = 0 ; haystack[i] != '\0'; i++)
 		{
 			min = i;
-			for ( j = 0 ; needle[j] != '\0'; j++)
+			for (j = 0 ; needle[j] != '\0'; j++)
 			{
 				if (needle[j] == haystack[i] && haystack[i] != '\0')
 				{
