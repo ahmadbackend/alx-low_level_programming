5069bb1377a238921796591973928844f9031125
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-29 01:31:14 +0300

    hy

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *accept)
 			for (j = 0 ; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
-					return (s[i]);
+					return (s + i);
 			}
 		}
 		return (0);
