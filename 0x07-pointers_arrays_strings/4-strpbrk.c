09de618ad03b424dcdaacfb45f168ae4e2c9f2c1
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-29 01:28:19 +0300

    :tr

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,9 +6,8 @@
 */
 char *_strpbrk(char *s, char *accept)
 	{
-		unsigned int i, j, min;
-		
-		min = 10000;
+		unsigned int i, j;
+
 		for (i = 0 ; s[i] != '\0'; i++)
 		{
 			for (j = 0 ; accept[j] != '\0'; j++)
