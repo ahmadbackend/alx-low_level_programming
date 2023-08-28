e65b86a21490709075d1e8e9ea7f739cfe41a759
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-29 00:54:04 +0300

    Htr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,10 @@ char *_strchr(char *s, char c)
 	{
 		int i;
 
-		i = 0;
-		while (s[i] != '\0')
+		for (i = 0; s[i] >= '\0'; i++)
 		{
-			if (*(s + i) == c)
+			if (s[i] == c)
 				return (s + i);
-			i++;
 		}
 		return ('\0');
 	}
