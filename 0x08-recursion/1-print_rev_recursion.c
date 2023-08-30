265f29eb07157765b0604bfcbe0128f5238149a3
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-30 23:14:59 +0300

    Gtr

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - check the code
+ *@s: string pointer
+ * Return: Always 0.
+*/
+void _print_rev_recursion(char *s)
+	{
+		if (*s == '\0')
+		{
+			_putchar('\n');
+			return;
+		}
+		s = s + 1;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
