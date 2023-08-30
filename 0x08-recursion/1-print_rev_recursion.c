ec5e3bff6967fcb73694c29f71da0526d2298f85
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-30 23:34:10 +0300

    Hyt

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,9 @@
 */
 void _print_rev_recursion(char *s)
 	{
-		if (*s != '\0')
+		if (*s > '\0')
 		{
-			s = s + 1;
-			_print_rev_recursion(s);
+			_print_rev_recursion(s + 1);
+			_putchar(*s);
 		}
-		_putchar(*s);
 	}
