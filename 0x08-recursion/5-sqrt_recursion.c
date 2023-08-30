7930d9a4b4369a333d510e8f51600a6b4e0cf715
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-31 01:23:40 +0300

    sqaure root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+/**
+ * test = internal method
+ *@k : the number
+ *@i: the root
+ *Return: the root
+*/
+int test(int i, int k)
+	{
+		if (i * i < k)
+		{
+			i = i + 1;
+			return(test(i, k));
+		}
+		else if (i * i == k)
+			return (i);
+		return (-1);
+	}
+
+/**
+ * _sqrt_recursion - get square root by recursion
+ *@n: the desired number
+ * Return: sqaure root
+*/
+int _sqrt_recursion(int n)
+	{
+		return (test(1, n));
+	}
