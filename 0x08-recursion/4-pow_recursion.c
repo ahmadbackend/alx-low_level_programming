f9053cd35496fdf241239375299e9a1540bf0412
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-08-31 00:15:52 +0300

    factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - use recursion as power
+ *@x: the number
+ *@y: the power
+ * Return: return s to power of y
+*/
+int _pow_recursion(int x, int y)
+	{
+		if (y > 0)
+			return ( x * _pow_recursion(x, y-1));
+		else if (y == 0)
+			return (1);
+		else
+			return (-1);
+	}
