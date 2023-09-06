4d832f60b41449d9c9fa28f8bb5d3ae7a5c443d2
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-09-07 02:41:49 +0300

    multiplcation

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ *@argc: number of arguments
+ *@argv: vector of arguments
+ * Return: always success
+*/
+int main(int argc, char *argv[])
+	{
+		int multi;
+		
+		if (argc != 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		multi = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multi);
+		return (0);
+	}
