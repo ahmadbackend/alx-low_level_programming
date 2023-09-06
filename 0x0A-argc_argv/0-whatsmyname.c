f2c4acdf71c26dd3d721dc78e940488eace54eef
ahmadbackend <ahmadbasherjsphp@gmail.com>
2023-09-07 02:08:22 +0300

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ *@agrc: number of arguments
+ *@argv: vector of arguments
+*/
+int main(int argc, char *argv[])
+	{
+		printf("%s\n", argv[argc-argc]);
+		return (0);
+	}
